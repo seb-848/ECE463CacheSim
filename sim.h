100c7ae7bee17f59ffc6c8a18714cbcb6a888249
SebMusic <sebastianecheverry848@gmail.com>
2025-09-03 10:53:33 -0400

    basic cache class

--- sim.h
+++ sim.h
@@ -14,4 +14,23 @@ struct {
 
 // Put additional data structures here as per your requirement.
 
+// cache class
+class Cache {
+   public:
+   cache_params_t parameters;
+
+   // Constructor
+   Cache(cache_params_t param) {
+      parameters.BLOCKSIZE = param.BLOCKSIZE;
+      parameters.L1_SIZE = param.L1_SIZE;
+      parameters.L1_ASSOC = param.L1_ASSOC;
+      parameters.L2_SIZE = param.L2_SIZE;
+      parameters.L2_ASSOC = param.L2_ASSOC;
+      parameters.PREF_N = param.PREF_N;
+      parameters.PREF_M = param.PREF_M;
+   }
+
+   // Destructor
+   ~Cache();
+}
 #endif
